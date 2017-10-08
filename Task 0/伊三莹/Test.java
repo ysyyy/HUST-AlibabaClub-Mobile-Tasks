aaf3e0cbfe2c74c6ab05cf92bfa8138ef35b2bde
ysyyy <32272220+ysyyy@users.noreply.github.com>
2017-10-09 00:32:43 +0800

    work

---
+++ Task 0/伊三莹/Test.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test{
+
+    public static void main(String[] args)
+    {
+        String myname="Sanying Yi";
+        System.out.printf("Hello %s",myname);
+    }
+
+
+}
